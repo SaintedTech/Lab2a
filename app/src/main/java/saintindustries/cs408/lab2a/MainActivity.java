c5b63cab5e16c9d20243ee7d1a87a379dd3dd55e
William Saint <wsaint@stu.jsu.edu>
2025-01-23 13:10:47 -0600

    First Commit

---
+++ app/src/main/java/saintindustries/cs408/lab2a/MainActivity.java
@@ -0,0 +1,24 @@
+package saintindustries.cs408.lab2a;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import saintindustries.cs408.lab2a.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
+
